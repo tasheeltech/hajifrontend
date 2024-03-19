7be53a6ad320a35c9ecb7d3aa31138ff5f3ffda0
Haseem Isaac <haseem97@gmail.com>
2024-03-19 18:18:15 +0000

    chore: how ot use

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,12 +1,17 @@
 import { useLoaderData, useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 import { useUserState } from "../helper/userStateHelper";
 import { DefaultLoader } from "../loaders/defaultLoader";
+import { useEffect } from "react";
 
 export default function Root() {
     const loadedData = useLoaderData() as DefaultLoader;
     const { micPermission, locationPermission, isoLanguage, name, setMicPermission, setLocationPermission, setIsoLanguage, setName, getUserScreen, computeUserScreen } = useUserState(loadedData);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const newName = "Bob"
+        computeUserScreen(micPermission, locationPermission, isoLanguage, newName)
+    })
 
     return (
         <>
