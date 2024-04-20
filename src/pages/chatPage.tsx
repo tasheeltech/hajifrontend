f4eadec75cdf1e43e85bfa1ff0d3872d19ff53af
Rafiudeen <126104518+Rafi-deen@users.noreply.github.com>
2024-04-20 20:38:45 +0530

    fix: TypeWriter effect error fixed

--- src/pages/chatPage.tsx
+++ src/pages/chatPage.tsx
@@ -208,6 +208,9 @@ export default function ChatPage() {
       console.log(res.payload.message)
       setAnswer("Please try again")
     }
+    if (res.payload.input === "undefined") {
+      setQuestion("?")
+    }
 
     if (res.type.startsWith("INTENT:NEAREST")) {
       setMapLocations(res.payload.locations.places)
