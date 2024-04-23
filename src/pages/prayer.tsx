bd4618622472ab6f6e6632a06232be372440417e
jubair <154318943+jubair445@users.noreply.github.com>
2024-04-23 19:07:15 +0530

    fixed prayer settings

--- src/pages/prayer.tsx
+++ src/pages/prayer.tsx
@@ -1,66 +1,106 @@
-import React, { useState } from "react"
-import { useLoaderData } from "react-router-dom"
-import { useUserState } from "../helper/userStateHelper"
-import { DefaultLoader } from "../loaders/defaultLoader"
-import { t } from "i18next"
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { useUserState } from "../helper/userStateHelper";
+import { DefaultLoader } from "../loaders/defaultLoader";
+import { t } from "i18next";
+
+const CalculationMethod = [
+  "MuslimWorldLeague",
+  "Egyptian",
+  "Karachi",
+  "UmmAlQura",
+  "Dubai",
+  "Qatar",
+  "Kuwait",
+  "MoonsightingCommittee",
+  "Singapore",
+  "Turkey",
+  "Tehran",
+  "NorthAmerica",
+];
 
 function Prayer() {
-  const loadedData = useLoaderData() as DefaultLoader
-  const { madhab, setMadhab } = useUserState(loadedData)
+  const loadedData = useLoaderData() as DefaultLoader;
+  const { madhab, setMadhab, calMethod, setCalMethod } =
+    useUserState(loadedData);
 
-  function getMadhab() {
-    if (madhab === "Hanafi") {
-      return false
-    } else {
-      return true
-    }
-  }
+  const [showMadhabOptions, setShowMadhabOptions] = useState(false);
+  const [showCalMethodOptions, setShowCalMethodOptions] = useState(false);
+  const [calMethodindex, setcalMethodindex] = useState(0 as number);
+  const handleMadhabClick = (madhab: string) => {
+    setMadhab(madhab);
+    setShowMadhabOptions(false);
+  };
 
-  const [shafiOrHanafi, setShafiOrHanafi] = useState(() => getMadhab())
+  const handleCalMethodClick = (calMethod: string) => {
+    setCalMethod(calMethod);
+    setcalMethodindex(parseInt(calMethod));
+    setShowCalMethodOptions(false);
+  };
 
-  const handleClick = (button: string) => {
-    setMadhab(button)
-  }
+  const handleToggleMadhabOptions = () => {
+    setShowMadhabOptions(!showMadhabOptions);
+  };
+
+  const handleToggleCalMethodOptions = () => {
+    setShowCalMethodOptions(!showCalMethodOptions);
+  };
 
   return (
-    <div className="flex flex-col text-center gap-12 justify-center items-center h-full m-6">
-      <div>
-        <p className="text-2xl font-medium text-[#6a6a6a] ">
-          {t("selectMadhab")}
-        </p>
-      </div>
-      <div className="flex gap-6 justify-around w-full">
-        <button
-          //   className="bg-[#2bce98] text-white font-bold py-2 px-4 rounded mr-4"
-          className={`${
-            shafiOrHanafi ? "bg-[#0b35276b] text-white" : "bg-[#2BCE986B]"
-          } shadow-[0_4px_4px_-1px_rgba(0,0,0,0.5)] rounded-[30px] px-4 py-3 flex items-center justify-center h-12 w-1/2 max-w-40 font-semibold`}
-          onClick={() => {
-            handleClick("Hanafi")
-            setShafiOrHanafi(false)
-            console.log(shafiOrHanafi, "hana")
-            console.log(madhab)
-          }}
-        >
-          {t("hanafi")}
-        </button>
-        <button
-          //   className="bg-[#2bce98] text-white font-bold py-2 px-4 rounded"
-          className={`${
-            shafiOrHanafi ? "bg-[#2BCE986B]" : "bg-[#0b35276b] text-white"
-          } shadow-[0_4px_4px_-1px_rgba(0,0,0,0.5)] rounded-[30px] px-4 py-3 flex items-center justify-center h-12 w-1/2 max-w-40 font-semibold`}
-          onClick={() => {
-            handleClick("Shafi")
-            setShafiOrHanafi(true)
-            console.log(shafiOrHanafi, "shaf")
-            console.log(madhab)
-          }}
-        >
-          {t("shafi")}
-        </button>
+    <div>
+      <h1 className="text-4xl text-center mt-6  mb-12">Prayer Settings</h1>
+      <div className="flex flex-col text-center gap-24 justify-center items-center h-full m-6">
+        <div>
+          <button
+            className="flex justify-center items-center py-4 px-8 rounded-md text-xl  gap-4 font-medium  bg-gray-600  text-center text-white "
+            onClick={handleToggleMadhabOptions}
+          >
+            {t("selectMadhab")}
+          </button>
+          <div className="flex flex-col justify-center items-center  w-full">
+            {["Hanafi", "Shafi", "Hambali", "Maliki"].map(
+              (madhabName, index) => (
+                <button
+                  key={index}
+                  className={`${
+                    madhab === madhabName
+                      ? "bg-[#0b35276b] text-white"
+                      : "bg-white"
+                  } px-4 py-3 flex items-center justify-center h-12 border-[1px] w-full font-semibold`}
+                  onClick={() => handleMadhabClick(madhabName)}
+                >
+                  {t(madhabName)}
+                </button>
+              )
+            )}
+          </div>
+        </div>
+        <div>
+          <button
+            className="flex justify-center items-center  py-4 px-8 rounded-md text-xl font-medium bg-gray-600 text-center text-white "
+            onClick={handleToggleCalMethodOptions}
+          >
+            select prayer calculation method
+          </button>
+          <div className="flex flex-col justify-center items-center w-full overflow-y-scroll max-h-48">
+            {CalculationMethod.map((calMethodName, index) => (
+              <button
+                key={index}
+                className={`${
+                  calMethod === calMethodName
+                    ? "bg-[#0b35276b] text-white"
+                    : "bg-white"
+                } px-4 py-3 flex items-center justify-center h-12 border-[1px] w-full font-semibold`}
+                onClick={() => handleCalMethodClick(calMethodName)}
+              >
+                {t(calMethodName)}
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Prayer
+export default Prayer;
